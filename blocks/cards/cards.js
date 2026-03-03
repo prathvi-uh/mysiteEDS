f6d69561183d20513a6f2498523dec10c387510b
Prathvi <prathviu@adobe.com>
2026-03-03 10:30:29 +0530

    changes made

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,5 +1,4 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
-
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -3,16 +3,13 @@
  * Include content on a page as a fragment.
  * https://www.aem.live/developer/block-collection/fragment
  */
-
 // eslint-disable-next-line import/no-cycle
 import {
   decorateMain,
 } from '../../scripts/scripts.js';
-
 import {
   loadSections,
 } from '../../scripts/aem.js';
-
 /**
  * Loads a fragment.
  * @param {string} path The path to the fragment
@@ -24,7 +21,6 @@ export async function loadFragment(path) {
     if (resp.ok) {
       const main = document.createElement('main');
       main.innerHTML = await resp.text();
-
       // reset base path for media to fragment base
       const resetAttributeBase = (tag, attr) => {
         main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
@@ -33,7 +29,6 @@ export async function loadFragment(path) {
       };
       resetAttributeBase('img', 'src');
       resetAttributeBase('source', 'srcset');
-
       decorateMain(main);
       await loadSections(main);
       return main;
@@ -41,7 +36,6 @@ export async function loadFragment(path) {
   }
   return null;
 }
-
 export default async function decorate(block) {
   const link = block.querySelector('a');
   const path = link ? link.getAttribute('href') : block.textContent.trim();
